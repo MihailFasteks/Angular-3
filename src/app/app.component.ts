07008c9ec79d3cebbca400c65a1c75ef8a915859
MihailFasteks <fastakovskiy.mihail04@gmail.com>
2024-05-19 23:00:09 +0300

    commited

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ContentComponent } from './content/content.component';
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, ContentComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'ProdCards';
+}
